82ce3cc739e277768a5b56d62796d32feeb6e5da
marvin hazael <marvinhazael@MacBook-Air-de-marvin.local>
2021-12-07 13:38:40 +0100

    password

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,6 +2,6 @@ const UserModel = require('../models/user.model');
 const ObjectId = require('mongoose').Types.ObjectId;//pour controler les ID sont reconnu par la base de donnée
 
 module.exports.getAllUsers = async (req, res) => {
-    const users = await UserModel.find().select();
+    const users = await UserModel.find().select('-password');//là tu me selectionne toutes les informations des users SAUF password !
     res.status(200).json(users);
 }
