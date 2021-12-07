9568615075e6ef4c71e5f1e8ce9035ffed559142
marvin hazael <marvinhazael@MacBook-Air-de-marvin.local>
2021-12-07 19:11:49 +0100

    delete.user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,17 +1,17 @@
 const UserModel = require('../models/user.model');
-const ObjectId = require('mongoose').Types.ObjectId;//pour controler les ID sont reconnu par la base de donnée
+const ObjectID = require('mongoose').Types.ObjectId;//pour controler les ID sont reconnu par la base de donnée
 
 
 //--------CRUD
-module.exports.getAllUsers = async (req, res) => {
+module.exports.getAllUsers = async (req, res, next) => {
     const users = await UserModel.find().select('-password');//là tu me selectionne toutes les informations des users SAUF password !
     res.status(200).json(users);
 }
 
-module.exports.userInfo = async (req, res) => {
+module.exports.userInfo = async (req, res, next) => {
     console.log(req.params);
-    if (!ObjectId.isValid(req.params.id))
-        return res.status(400).send('ID unknown : ' + req.params.id)
+    if (!ObjectID.isValid(req.params.id))
+        return res.status(400).send('ID unknown : ' + req.params.id) //c'est pour controller que l'iD est connu 
     
     UserModel.findById(req.params.id, (err,docs) => {
         if(!err) res.send(docs);
@@ -19,4 +19,37 @@ module.exports.userInfo = async (req, res) => {
     }).select('-password');
 };
 
-module.exports.u
+// module.exports.updateUser = async (req, res, next) => {
+//     if (!ObjectID.isValid(req.params.id))//on vérifie si le ID est correct
+//         return res.status(400).send('ID unknown : ' + req.params.id)
+
+//     try {
+//         await UserModel.updateOne(
+//             {_id : req.params.id},
+//             {
+//                 $set: {
+//                     bio: req.body.bio,
+//                 },
+//             },
+//             { new: true, upsert: true, setDefaultsOnInsert: true },
+//             (err, docs) => {
+//                 if (!err) return res.send(docs);
+//                 if (err) return res.status(500).send({ message: err });
+//             }
+//         )
+//     } catch (err) {
+//         return res.status(500).json({ message: err });
+//     }
+// };
+
+module.exports.deleteUser = async (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+      return res.status(400).send("ID unknown : " + req.params.id);
+  
+    try {
+      await UserModel.deleteOne({ _id: req.params.id }).exec();
+      res.status(200).json({ message: "Successfully deleted. " });
+    } catch (err) {
+      return res.status(500).json({ message: err });
+    }
+  };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -8,7 +8,7 @@ router.post("/register", authController.signUp);
 // user display: 'block',
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.userInfo);
-
-
+//router.put('/:id', userController.updateUser);
+router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
